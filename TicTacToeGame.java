e347a3bacf94d813c7b4e293f63caea722edd9c8
neil245 <indranilghoshal5@gmail.com>
2021-05-18 08:41:09 +0530

    Files added

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,19 @@
 package com.TicTacToe.java;
+class CreateBoard {
+	//public char board[] = new char[10];
+	public char board[] = new char[10];
+	void createBoard() {
+	for (int i=0; i<10; i++) {
+		board[i] = ' ' ;
+	}
+}
+}
 
 public class TicTacToeGame {
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		CreateBoard obj1 = new CreateBoard();
+		obj1.createBoard();
+	}
 
 }
